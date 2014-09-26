cf9473579033c27ad603d1d8d50284079192b45b
gnustomp <lolz836@gmail.com>
2014-09-26 14:24:08 +1000

    chan-mgmt: use ninjabot API

--- plugins/misc/chan-mgmt.py
+++ plugins/misc/chan-mgmt.py
@@ -5,7 +5,7 @@ class Plugin(object):
 		self.bot = bot
 	
 	def trigger_kick(self, msg):
-		"Kicks nicks from channels. RFC-style format. When used in a channel message, channel to kick from is that channel."
+		"Kicks users from channels. RFC-style format. When used in a channel message, channel to kick from is that channel."
 		if not self.bot.is_admin(msg.nick):
 			return
 
@@ -15,25 +15,28 @@ class Plugin(object):
 				self.bot.notice(msg.nick, "There must be at least one channel parameter and one user parameter")
 				return
 			channels = msg.args.pop(0).split(",")
-			nicks = msg.args.pop(0).split(",")
+			users = msg.args.pop(0).split(",")
 			if len(channels) > 1:
-				if len(channels) != len(nicks):
+				if len(channels) != len(users):
 					self.bot.notice(msg.nick, "If there are multiple channel parameters, there must be as many user parameters")
 					return
 			comment = ""
 			if msg.args:
 				print(msg.args)
 				comment = " ".join(msg.args)
-			self.bot.irc_send("KICK {0} {1} :{2}".format(",".join(channels), ",".join(nicks), comment))
 
 		else:
 			# If the message is received from a channel, omit the channel.
-			nicks = msg.args.pop(0)
+			channels = msg.channel
+			users = msg.args.pop(0)
 			comment = ""
 			if msg.args:
 				comment = " ".join(msg.args)
 
-			self.bot.irc_send("KICK {0} {1} :{2}".format(msg.channel, nicks, comment))
+		if comment:
+			self.bot.kick(channels, users, comment=comment)
+		else:
+			self.bot.kick(channels, users)
 
 	def trigger_mode(self, msg):
 		"Sets modes on a channel"
