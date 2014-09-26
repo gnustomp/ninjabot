363c5b14e05bde3414d8cfa3653a73da6a71ddc2
gnustomp <lolz836@gmail.com>
2014-09-26 13:21:35 +1000

    chan-mgmt: improvements

--- plugins/misc/chan-mgmt.py
+++ plugins/misc/chan-mgmt.py
@@ -1,53 +1,46 @@
+# Channel management plugin. Bot must be operator in the channels you wish to manage.
+
 class Plugin(object):
 	def load(self, bot, config):
 		self.bot = bot
 	
-	def trigger_rkick(self, msg):
-		if not self.bot.is_admin(msg.nick):
-			return
-
-		if len(msg.args) < 2:
-			self.bot.notice(msg.nick, "u dun goofd")
-			return
-		channels = msg.args.pop(0).split(",")
-		nicks = msg.args.pop(0).split(",")
-		if len(channels) > 1:
-			if len(channels) != len(nicks):
-				self.bot.notice(msg.nick, "must equal no")
-				return
-		comment = ""
-		if msg.args:
-			print(msg.args)
-			comment = " ".join(msg.args)
-		self.bot.irc_send("KICK {0} {1} :{2}".format(",".join(channels), ",".join(nicks), comment))
-
 	def trigger_kick(self, msg):
-		if msg.type != 1:
-			self.bot.notice(msg.nick, "kick must be used in a channel. Use rkick otherwise")
-			return
-
+		"Kicks nicks from channels. RFC-style format. When used in a channel message, channel to kick from is that channel."
 		if not self.bot.is_admin(msg.nick):
 			return
 
-		nicks = msg.args.pop(0)
-		comment = ""
-		if msg.args:
-			comment = " ".join(msg.args)
+		if msg.type != 1:
+			# If message is not received from a channel, use RFC-style syntax.
+			if len(msg.args) < 2:
+				self.bot.notice(msg.nick, "There must be at least one channel parameter and one user parameter")
+				return
+			channels = msg.args.pop(0).split(",")
+			nicks = msg.args.pop(0).split(",")
+			if len(channels) > 1:
+				if len(channels) != len(nicks):
+					self.bot.notice(msg.nick, "If there are multiple channel parameters, there must be as many user parameters")
+					return
+			comment = ""
+			if msg.args:
+				print(msg.args)
+				comment = " ".join(msg.args)
+			self.bot.irc_send("KICK {0} {1} :{2}".format(",".join(channels), ",".join(nicks), comment))
 
-		self.bot.irc_send("KICK {0} {1} :{2}".format(msg.channel, nicks, comment))
+		else:
+			# If the message is received from a channel, omit the channel.
+			nicks = msg.args.pop(0)
+			comment = ""
+			if msg.args:
+				comment = " ".join(msg.args)
+
+			self.bot.irc_send("KICK {0} {1} :{2}".format(msg.channel, nicks, comment))
 
 	def trigger_mode(self, msg):
+		"Sets modes on a channel"
 		if not self.bot.is_admin(msg.nick):
 			return
 
-		if len(msg.args) < 2:
-			channel = msg.channel
-		else:
-			channel = ' '.join(msg.args)
-		if len(channel.split(",")) > 1:
-			self.bot.notice(msg.nick, "Can only specify one channel")
-			return
-
-		modes = msg.args.pop(0)
+		channel = msg.args.pop(0)
+		modes = ' '.join(msg.args)
 		self.bot.irc_send("MODE {0} {1}".format(channel, modes))
 
